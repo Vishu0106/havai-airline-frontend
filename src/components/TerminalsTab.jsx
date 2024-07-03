17f6f76ea5434eb7a55192bb01b173ac8723892b
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-07-03 18:27:09 +0530

    Added all Components from react-specturm

--- src/components/TerminalsTab.jsx
+++ src/components/TerminalsTab.jsx
@@ -1,7 +1,7 @@
 import TerminalCard from "./TerminalCard"
 function TerminalsTab() {
   return (
-   <div className="min-h-[19vh] w-auto flex gap-8 p-4">
+   <div className="min-h-[18vh] w-auto flex gap-8 p-4 ">
         <TerminalCard value="1"/>
         <TerminalCard value="2"/>
    </div>
